2673deceb2e5ac89ab0b13a94e17c73a513e0c27
Angel Gonzalez <angeledugo@gmail.com>
2022-06-14 20:43:09 -0500

    creacion de controladores y actualizacion de controladore type

---
+++ app/Http/Controllers/TypeController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Type;
+
+class TypeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+
+        $types = Type::all();
+
+        return response()->json($types);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $type = Type::create($request->post());
+
+        return response()->json([
+            'type' => $type,
+
+        ])
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Type $tpye)
+    {
+        //
+        return response()->json($type);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Type $type)
+    {
+        //
+        $type->fill($request->post())->save();
+
+        return response()->json([
+            'type' => $blog
+        ])
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Type $type)
+    {
+        //
+        $type->delete();
+
+        return response()->json([
+            'mensaje' => 'Tipo eliminado'
+        ]);
+    }
+}
